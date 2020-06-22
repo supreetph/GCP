3472bdfefe0cf91115ef578ea9910c70af24dc78
Supreet <suprith.prakash.sp@gmail.com>
2020-06-22 19:57:09 +0530

    cloud talent methods added

--- PubSubdemp/Program.cs
+++ PubSubdemp/Program.cs
@@ -4,7 +4,10 @@ using Google.Protobuf;
 using System;
 using System.IO;
 using System.Linq;
+using Google.Apis.CloudTalentSolution.v3p1beta1;
 using System.Threading.Tasks;
+using Google.Apis.CloudTalentSolution.v3p1beta1.Data;
+using Google.Cloud.Talent.V4Beta1;
 
 namespace PubSubdemp
 {
@@ -21,7 +24,31 @@ namespace PubSubdemp
             Environment.SetEnvironmentVariable(
        "GOOGLE_APPLICATION_CREDENTIALS",
        Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "My First Project-065d320bab2e.json"));
-           // GoogleCredential credential = GoogleCredential.GetApplicationDefault();
+            CompanyServiceClient companyServiceClient = CompanyServiceClient.Create();
+            // GoogleCredential credential = GoogleCredential.GetApplicationDefault();
+            Google.Cloud.Talent.V4Beta1.Company c = new Google.Cloud.Talent.V4Beta1.Company
+            {
+                Name = "ADI",
+                DisplayName="ADI",
+               
+                ExternalId="123"
+
+
+            };
+            Google.Cloud.Talent.V4Beta1.CreateCompanyRequest request = new Google.Cloud.Talent.V4Beta1.CreateCompanyRequest
+            {
+               
+                Company = c
+            };
+            
+            Google.Cloud.Talent.V4Beta1.Company response = companyServiceClient.CreateCompany(request);
+            Console.WriteLine("Created Company");
+            Console.WriteLine($"Name: {response.Name}");
+            Console.WriteLine($"Display Name: {response.DisplayName}");
+            Console.WriteLine($"External ID: {response.ExternalId}");
+
+            //JobServiceClient jobServiceClient = JobServiceClient.Create();
+            // TenantName tenantName = TenantName.FromProjectTenant(projectId, tenantId);
             PublisherClient publisher = await PublisherClient.CreateAsync(topicName, null, null);
             var pubsubMessage = new PubsubMessage()
             {
@@ -29,6 +56,27 @@ namespace PubSubdemp
 
             };
             string messageId = await publisher.PublishAsync( pubsubMessage);
+            var subscriptionName = new SubscriptionName(ProjectId, SubscriptionId);
+            var subscription = await SubscriberClient.CreateAsync(subscriptionName);
+            try
+            {
+               // PullResponse response = subscription.(subscriptionName, true, 10);
+                await subscription.StartAsync((pubsubMessage, cancellationToken) =>
+                {
+                    // Process the message here.
+                    var all= Task.FromResult(SubscriberClient.Reply.Ack);
+                    return all;
+                });
+
+                
+
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong: {0}", e.Message);
+               
+            }
         }
        
   
